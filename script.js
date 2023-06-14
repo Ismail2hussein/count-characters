f750213eb5563a72f9ea030d90c94e0b8e06ab35
Ismail2hussein <ihusseinmohamed629@gmail.com>
2023-06-14 13:50:06 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-// Good Luck 💪🏾
+function count(char, str) {
+  let count = 0;
+  let arr = str.split("");
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === char) {
+      count++;
+    }
+  }
+  return count;
+}
+console.log(count("a", "fikrcamp")); // Output: 1
+console.log(count("c", "bootcamp")); // Output: 1
+console.log(count("o", "fikrcamp coding bootcamp")); // Output: 3
